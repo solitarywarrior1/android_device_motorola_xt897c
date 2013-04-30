98db2912216a614bf1aedcea984aa4abcb30ae5b
arrrghhh <arrrghhh@xdandroid.com>
2013-04-29 18:42:17 -0600

    Add backslashes...

--- device_xt897c.mk
+++ device_xt897c.mk
@@ -37,9 +37,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnCdmaDevice=1 \
     ro.telephony.ril.v3=signalstrength \
     DEVICE_PROVISIONED=1 \
-    ro.telephony.default_network=8
-    ro.cdma.home.operator.numeric=310120
-    ro.cdma.home.operator.alpha=Sprint
+    ro.telephony.default_network=8 \
+    ro.cdma.home.operator.numeric=310120 \
+    ro.cdma.home.operator.alpha=Sprint \
     ro.config.svlte1x=true
 
 # IDC
